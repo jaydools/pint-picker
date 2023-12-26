ba39459345a3e1ae45e2ef96bbbff84b1554094f
jaydools <134211868+jaydools@users.noreply.github.com>
2023-12-26 12:51:39 -0500

    basic layout made with some palceholder text and a button

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -2,7 +2,21 @@ import React from "react";
 import "./homepage.scss";
 
 function homepage() {
-    return <div className="background">homepag</div>;
+    return (
+        <section className="background">
+            <h1 className="title-text">Help me choose a beer</h1>
+            <div className="subhead">
+                <p className="body-text">Not sure what you want to drink?</p>
+                <p className="body-text">
+                    No worries! Let's get to know what you like and I'll suggest something for you.
+                </p>
+            </div>
+            <div className="subhead">
+                <h2 className="subhead__text">Start the beer finder quiz below</h2>
+            </div>
+            <button className="button">Let's find you a beer</button>
+        </section>
+    );
 }
 
 export default homepage;
